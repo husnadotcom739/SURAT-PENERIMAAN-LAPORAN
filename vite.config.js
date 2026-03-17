2e4a618e8bea8219ed8694e54c65bb1154588105
SUHILMANartz <147611226+SUHILMANartz@users.noreply.github.com>
2026-03-17 10:09:55 +0700

    Fix: add base path for gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/SURAT-PENERIMAAN-LAPORAN/', // Samakan dengan nama repository GitHub kamu
+  base: '/SURAT-PENERIMAAN-LAPORAN/', // WAJIB: Sesuaikan dengan nama repo kamu
 })
